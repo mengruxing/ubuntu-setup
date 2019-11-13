0b0949ca63b0410afa7333ad788acb7ce65e09f6
mengruxing <mengruxing@gmail.com>
2019-11-13 10:09:49 +0800

    add nvidia-container-runtime

--- 12_nvidia-docker.sh
+++ 12_nvidia-docker.sh
@@ -8,8 +8,22 @@ curl -sL https://nvidia.github.io/nvidia-docker/$(. /etc/os-release;echo $ID$VER
 curl -sL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker-stable.list
 sudo apt-get update
-sudo apt-get install -y docker-ce nvidia-container-toolkit
+sudo apt-get install -y docker-ce nvidia-container-toolkit nvidia-container-runtime
 sudo usermod -a -G docker $(awk -F: '$3==1000 {print $1}' /etc/passwd)
+if [ -e /etc/docker/daemon.json ]; then
+    echo '/etc/docker/daemon.json file existed, please add nvidia-container-runtime manually.'
+else
+    sudo tee /etc/docker/daemon.json << EOF
+{
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+fi
 sudo systemctl restart docker
 
 sudo cp ./files/docker-compose /usr/local/bin/docker-compose
