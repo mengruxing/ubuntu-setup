6b0c1e901cd1397f08d73894c7faea6714105255
mengruxing <mengruxing@gmail.com>
2019-12-03 14:06:15 +0800

    add to remove desktop apps

--- 12_nvidia-cuda.sh
+++ 12_nvidia-cuda.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 cd `dirname $0`
+
+if [ -f tmp/config.sh ]; then
+  source tmp/config.sh
+else
+  echo "missing file: tmp/config.sh, please edit it and retry.."
+  exit 1
+fi
+
 sudo ./files/cuda_10.1.243_418.87.00_linux.run --silent --toolkit --toolkitpath=/opt/cuda-10.1
 if [ $? -ne 0 ]; then
     echo "cuda installing failed.."
@@ -17,3 +25,9 @@ sudo ldconfig
 cd /opt && sudo ln -sf cuda-10.1 cuda
 sudo rm /usr/local/cuda
 sudo ln -sf /opt/cuda /usr/local/
+
+if [[ "${nvidia_cuda_opts}" == *no*opengl* ]]; then
+  sudo rm /usr/share/applications/nvvp.desktop /usr/share/applications/nsight*.desktop
+else
+  echo 'pass'
+fi
