3ecca73a051538223e5fc70d15ece7d2761a0a3c
mengruxing <mengruxing@gmail.com>
2020-02-20 10:03:59 +0800

    sources.list replace from start

--- 00_apt-update.sh
+++ 00_apt-update.sh
@@ -5,5 +5,5 @@ cd `dirname $0`
 if [ ! -f ~/dpkg.list.txt ]; then
   dpkg --get-selections | cut -f 1 > ~/dpkg.list.txt
 fi
-sudo sed -i 's/\s*#\s*\(deb.*https\?\)/\1/g' /etc/apt/sources.list
+sudo sed -i 's/^\s*#\s*\(deb.*https\?\)/\1/g' /etc/apt/sources.list
 sudo apt-get update && sudo apt-get dist-upgrade -y && echo 'operation completed! system needs to reboot..'
