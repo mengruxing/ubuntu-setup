0f02179c575bd1fc2a27de8d112e24a3c66dbb84
mengruxing <mengruxing@gmail.com>
2020-03-06 14:40:38 +0800

    not to convert archive.canonical.com to cn.archive.canonical.com

--- 00_apt-update.sh
+++ 00_apt-update.sh
@@ -6,5 +6,5 @@ mkdir -p ~/backup/dpkg
 if [ ! -f ~/backup/dpkg/dpkg.list.txt ]; then
   dpkg --get-selections | cut -f 1 > ~/backup/dpkg/dpkg.list.txt
 fi
-sudo sed -i 's/^\s*#\s*\(deb.*https\?\)/\1/g;s/\(https\?:\/\/\)\(archive.\(ubuntu\|canonical\).com\)/\1cn.\2/g' /etc/apt/sources.list
+sudo sed -i 's/^\s*#\s*\(deb.*https\?\)/\1/g;s/\(https\?:\/\/\)\(archive\.ubuntu\.com\)/\1cn\.\2/g' /etc/apt/sources.list
 sudo apt-get update && sudo apt-get dist-upgrade -y && echo 'operation completed! system needs to reboot..'
