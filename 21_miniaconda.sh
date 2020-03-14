f05968f00d1a5b22e8ea862ea5be8347fc42320f
mengruxing <mengruxing@gmail.com>
2020-03-14 16:20:48 +0800

    add miniconda

---
+++ 21_miniaconda.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+./request-file.sh Miniconda2-4.7.12.1-Linux-x86_64.sh
+
+sudo ./local/Miniconda2-4.7.12.1-Linux-x86_64.sh -b -p /opt/miniconda2
+cd /opt && sudo ln -sf miniconda2 miniconda
+if [ ! -d /etc/profile.d/opt ]; then
+  sudo mkdir /etc/profile.d/opt
+fi
+sudo tee /etc/profile.d/opt/miniconda.sh << EOF
+export PATH=\$PATH:/opt/miniconda/bin
+EOF
